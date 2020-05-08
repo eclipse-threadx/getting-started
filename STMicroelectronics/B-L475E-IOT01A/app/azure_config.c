98eb94e70559e4240ab4fb5b4dc33cb29f7778ab
Ryan Winter <ryanwinter@outlook.com>
2020-05-08 14:14:07 -0700

    removed configuration

--- STMicroelectronics/B-L475E-IOT01A/app/azure_config.c
+++ STMicroelectronics/B-L475E-IOT01A/app/azure_config.c
@@ -3,18 +3,18 @@
 // ----------------------------------------------------------------------------
 // WiFi connection information
 // ----------------------------------------------------------------------------
-const char* wifi_ssid			= "ferret";
-const char* wifi_password       = "i am a furry animal";
+const char* wifi_ssid           = "";
+const char* wifi_password       = "";
 const WiFi_Mode wifi_mode       = WPA_Personal; // None, WEP, WPA_Personal
 
 // ----------------------------------------------------------------------------
 // Azure IoT Device information
 // ----------------------------------------------------------------------------
 // The IoT Hub hostname
-const char* iot_hub_hostname    = "azurertos-iot.azure-devices.net";
+const char* iot_hub_hostname    = "";
 
 // The device ID
-const char* iot_device_id       = "renesas_ae_cloud2";
+const char* iot_device_id       = "";
 
 // The devices base64 SAS key
-const char* iot_sas_key         = "bbatYjAS4QrmTd+2srQHFtGFfiSAzIzuB5YPIsnJkQ0=";
+const char* iot_sas_key         = "";
