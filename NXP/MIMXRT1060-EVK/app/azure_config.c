1a285007883ab66affdb65a6283ed2c8725ed536
Ryan Winter <ryanwinter@outlook.com>
2020-05-11 10:36:41 -0700

    Updates for rtos 6.0rc1 refresh

--- NXP/MIMXRT1060-EVK/app/azure_config.c
+++ NXP/MIMXRT1060-EVK/app/azure_config.c
@@ -1,10 +1,10 @@
 #include "azure_config.h"
 
 // The IoT Hub hostname
-const char* iot_hub_hostname    = "azurertos-iot.azure-devices.net";
+const char* iot_hub_hostname    = "";
 
 // The device ID
-const char* iot_device_id       = "renesas_ae_cloud2";
+const char* iot_device_id       = "";
 
 // The devices base64 SAS key
-const char* iot_sas_key         = "bbatYjAS4QrmTd+2srQHFtGFfiSAzIzuB5YPIsnJkQ0=";
+const char* iot_sas_key         = "";
