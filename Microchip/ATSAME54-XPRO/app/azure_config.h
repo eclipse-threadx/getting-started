971b855994e0d112c9f98ac00c318e8e356bba63
Mariana Mihova <marine@microsoft.com>
2020-05-13 14:11:40 -0700

    Remove iot hub connection credentials

--- Microchip/ATSAME54-XPRO/app/azure_config.h
+++ Microchip/ATSAME54-XPRO/app/azure_config.h
@@ -2,12 +2,8 @@
 #define _AZURE_CONFIG_H
 
 /*************** Azure IoT Hub Connection Configuration ******************/
-//#define IOT_HUB_HOSTNAME    ""
-//#define IOT_DEVICE_ID       ""
-//#define IOT_PRIMARY_KEY     ""
-
-#define IOT_HUB_HOSTNAME    "MarianaIoTHub.azure-devices.net"
-#define IOT_DEVICE_ID       "same54"
-#define IOT_PRIMARY_KEY     "FnmTF+SyPcUaj2wQjLYheTRKzpz6wYUbO/MosMsrUO4="
+#define IOT_HUB_HOSTNAME    ""
+#define IOT_DEVICE_ID       ""
+#define IOT_PRIMARY_KEY     ""
 
 #endif // _AZURE_CONFIG_H
