16d930d8b5112521c239e005c43fef58d2fe841e
Ryan Winter <ryanwinter@outlook.com>
2020-05-12 23:44:40 -0700

    NXP + STM MQTT updates for doc run-through

--- STMicroelectronics/B-L475E-IOT01A/app/azure_iothub.c
+++ STMicroelectronics/B-L475E-IOT01A/app/azure_iothub.c
@@ -4,36 +4,26 @@
 
 #include "tx_api.h"
 
-//#include "stm32l475xx.h"
-//#include "stm32l4xx.h"
-//#include "stm32l4xx_hal_gpio.h"
 #include "stm32l475e_iot01.h"
 
 #include "azure/azure_mqtt.h"
 
-//#define LED_PORT DT_GPIO_STM32_GPIOC_LABEL
-#define LED_PIN 9
-
-void mqtt_thread_entry(ULONG info);
+void set_led_state(bool level);
 void mqtt_direct_method_invoke(CHAR *direct_method_name, CHAR *message, MQTT_DIRECT_METHOD_RESPONSE *response);
 void mqtt_c2d_message(CHAR *key, CHAR *value);
+void mqtt_thread_entry(ULONG info);
 
-void mqtt_thread_entry(ULONG info)
+void set_led_state(bool level)
 {
-    printf("Starting MQTT thread\r\n");
-
-    while (true)
+    if (level)
     {
-        float tempDegC = 25.0;
-
-        // Send the compensated temperature as a telemetry event
-        azure_mqtt_publish_float_twin("temperature(C)", tempDegC);
-
-        // Send the compensated temperature as a device twin update
-        azure_mqtt_publish_float_telemetry("temperature(C)", tempDegC);
-
-        // Sleep for 1 minute
-        tx_thread_sleep(60 * TX_TIMER_TICKS_PER_SECOND);
+        printf("LED is turned ON\r\n");
+        BSP_LED_On(LED_GREEN);
+    }
+    else
+    {
+        printf("LED is turned OFF\r\n");
+        BSP_LED_Off(LED_GREEN);
     }
 }
 
@@ -41,23 +31,13 @@ void mqtt_direct_method_invoke(CHAR *direct_method_name, CHAR *message, MQTT_DIR
 {
     // Default response - 501 Not Implemented
     int status = 501;
-    if (strstr((CHAR *)direct_method_name, "set_led_state"))
+    if (strcmp((CHAR *)direct_method_name, "set_led_state") == 0)
     {
         // Set LED state
         // '0' - turn LED off
         // '1' - turn LED on
         int arg = atoi(message);
-
-        //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, arg);
-        if(arg == 0)
-        {
-            BSP_LED_Off(LED_GREEN);
-        }
-        else if (arg == 1)
-        {
-            BSP_LED_On(LED_GREEN);
-        }
-        else
+        if (arg != 0 && arg != 1)
         {
             printf("Invalid LED state. Possible states are '0' - turn LED off or '1' - turn LED on\r\n");
 
@@ -65,6 +45,8 @@ void mqtt_direct_method_invoke(CHAR *direct_method_name, CHAR *message, MQTT_DIR
             strcpy(response->message, "{}");
             return;
         }
+
+        set_led_state(arg);
         
         // 204 No Content, the server successfully processed the request and is not returning any content.
         status = 204;
@@ -84,7 +66,54 @@ void mqtt_direct_method_invoke(CHAR *direct_method_name, CHAR *message, MQTT_DIR
     return;
 }
 
-bool azure_iothub_start()
+void mqtt_c2d_message(CHAR *key, CHAR *value)
+{
+    if (strcmp((CHAR *)key, "led0State") == 0)
+    {
+        // Set LED state
+        // '0' - turn LED off
+        // '1' - turn LED on
+        int arg = atoi(value);
+        if (arg != 0 && arg != 1)
+        {
+            printf("Invalid LED state. Possible states are '0' - turn LED off or '1' - turn LED on\r\n");
+            return;
+        }
+        set_led_state(arg);
+
+        // Update device twin property
+        azure_mqtt_publish_bool_twin(key, arg);
+    }
+    else
+    {
+        // Update device twin property
+        azure_mqtt_publish_string_twin(key, value);
+    }
+    
+    printf("Property=%s updated with value=%s\r\n", key, value);
+}
+
+void mqtt_thread_entry(ULONG info)
+{
+    printf("Starting MQTT thread\r\n");
+
+    float temperature = 25.0;
+
+    while (true)
+    {
+
+        // Send the compensated temperature as a telemetry event
+        azure_mqtt_publish_float_twin("temperature", temperature);
+
+        // Send the compensated temperature as a device twin update
+        azure_mqtt_publish_float_telemetry("temperature", temperature);
+
+        // Sleep for 1 minute
+        tx_thread_sleep(10 * TX_TIMER_TICKS_PER_SECOND);
+    }
+}
+
+bool azure_mqtt_init()
 {
     bool status;
     status = azure_mqtt_register_main_thread_callback(mqtt_thread_entry);
@@ -101,12 +130,12 @@ bool azure_iothub_start()
         return status;
     }
     
-//    status = azure_mqtt_register_c2d_message_callback(mqtt_c2d_message);
-//    if (!status)
-//    {
-//        printf("Failed to register MQTT cloud to device callback callback\r\n");
-//        return status;
-//    }
+    status = azure_mqtt_register_c2d_message_callback(mqtt_c2d_message);
+    if (!status)
+    {
+        printf("Failed to register MQTT cloud to device callback callback\r\n");
+        return status;
+    }
     
     // Start the Azure MQTT client
     status = azure_mqtt_start();
--- STMicroelectronics/B-L475E-IOT01A/app/networking.c
+++ STMicroelectronics/B-L475E-IOT01A/app/networking.c
@@ -11,8 +11,8 @@
 
 #include "azure_config.h"
 
-#define THREADX_PACKET_COUNT (10)
-#define THREADX_PACKET_SIZE  (1200)  /* Set the default value to 1200 since WIFI payload size (ES_WIFI_PAYLOAD_SIZE) is 1200.  */
+#define THREADX_PACKET_COUNT (20)
+#define THREADX_PACKET_SIZE  (1200) // Set the default value to 1200 since WIFI payload size (ES_WIFI_PAYLOAD_SIZE) is 1200
 #define THREADX_POOL_SIZE    ((THREADX_PACKET_SIZE + sizeof(NX_PACKET)) * THREADX_PACKET_COUNT)
 
 #define WIFI_CONNECT_MAX_ATTEMPT_COUNT 5
@@ -35,7 +35,6 @@ static void print_address(CHAR* preable, uint8_t address[4])
 
 static bool wifi_init()
 {
-    char moduleinfo[ES_WIFI_MAX_SSID_NAME_SIZE];
     WIFI_Ecn_t security_mode;
 
     switch (wifi_mode)
@@ -52,9 +51,6 @@ static bool wifi_init()
             break;
     };
 
-    int32_t wifiConnectCounter = 1;
-    WIFI_Status_t result;
-
     printf("Initializing WiFi\r\n");
 
     if (wifi_ssid[0] == '\0')
@@ -67,15 +63,13 @@ static bool wifi_init()
 
     if (WIFI_Init() != WIFI_STATUS_OK)
     {
-        printf("Failed to initialize WIFI module\r\n");
+        printf("ERROR: Failed to initialize WIFI module\r\n");
         return false;
     }
 
-    // Retrieve the WiFi module mac address to confirm that it is detected and communicating.
-    WIFI_GetModuleName(moduleinfo);
-
     // Connect to the specified SSID
-    while ((result = WIFI_Connect(wifi_ssid, wifi_password, security_mode)) != WIFI_STATUS_OK)
+    int32_t wifiConnectCounter = 1;
+    while (WIFI_Connect(wifi_ssid, wifi_password, security_mode) != WIFI_STATUS_OK)
     {
         printf("\tWiFi is not able connect to '%s', attempt = %ld\r\n", wifi_ssid, wifiConnectCounter++);
         HAL_Delay(1000);
@@ -83,10 +77,11 @@ static bool wifi_init()
         // Max number of attempts
         if (wifiConnectCounter == WIFI_CONNECT_MAX_ATTEMPT_COUNT)
         {
-            printf("ERROR: WiFi is not able to connected to the '%s', with error %d\r\n", wifi_ssid, result);
+            printf("ERROR: WiFi is not able to connected to the '%s'\r\n", wifi_ssid);
             return false;
         }
     }
+
     printf("SUCCESS: WiFi connected to %s\r\n\r\n", wifi_ssid);
 
     printf("Initializing DHCP\r\n");
@@ -120,30 +115,36 @@ static UINT dns_create()
  
     printf("Initializing DNS client\r\n");
 
-    if (WIFI_GetDNS_Address(dns_address_1, dns_address_2) != WIFI_STATUS_OK)
-    {
-        printf("ERROR: Failed to create DNS\r\n");
-        return 1;
-    }
-    
     status = nx_dns_create(&dns_client, &ip_0, (UCHAR *)"DNS Client");
     if (status != NX_SUCCESS)
     {
+        printf("ERROR: Failed to create DNS (%0x02)\r\n", status);
         return status;
     }
 
     // Use the packet pool here
+#ifdef NX_DNS_CLIENT_USER_CREATE_PACKET_POOL 
     status = nx_dns_packet_pool_set(&dns_client, ip_0.nx_ip_default_packet_pool);
     if (status != NX_SUCCESS)
     {
+        printf("ERROR: Failed to create DNS packet pool (%0x02)\r\n", status);
         nx_dns_delete(&dns_client);
         return status;
     }
-    
+#endif
+
+    if (WIFI_GetDNS_Address(dns_address_1, dns_address_2) != WIFI_STATUS_OK)
+    {
+        printf("ERROR: Failed to fetch Wifi DNS\r\n");
+        nx_dns_delete(&dns_client);
+        return NX_NOT_SUCCESSFUL;
+    }
+
     // Add an IPv4 server address to the Client list.
     status = nx_dns_server_add(&dns_client, IP_ADDRESS(dns_address_1[0], dns_address_1[1], dns_address_1[2], dns_address_1[3]));
     if (status != NX_SUCCESS)
     {
+        printf("ERROR: Failed to add dns server (%0x02)\r\n", status);
         nx_dns_delete(&dns_client);
         return status;
     }
@@ -163,7 +164,7 @@ int stm32_network_init(void)
     // Intialize Wifi
     if (!wifi_init())
     {
-        return 1;
+        return NX_NOT_SUCCESSFUL;
     }
 
     // Initialize the NetX system.
@@ -175,7 +176,7 @@ int stm32_network_init(void)
         threadx_ip_pool, THREADX_POOL_SIZE);
     if (status != NX_SUCCESS)
     {
-        printf("ERROR: PACKET POOL CREATE FAIL.\r\n");
+        printf("ERROR: Packet pool create fail.\r\n");
         return status;
     }
 
@@ -183,11 +184,12 @@ int stm32_network_init(void)
      status = nx_ip_create(&ip_0, "NetX IP Instance 0",
         0, 0,
         &main_pool, NULL, 
-        NULL, 0, 0);
+        NULL, 0, 
+        0);
     if (status != NX_SUCCESS)
     {
         nx_packet_pool_delete(&main_pool);
-        printf("ERROR: IP CREATE FAIL.\r\n");
+        printf("ERROR: IP create fail.\r\n");
         return status;
     }
     
@@ -197,9 +199,12 @@ int stm32_network_init(void)
     {
         nx_ip_delete(&ip_0);
         nx_packet_pool_delete(&main_pool);
-        printf("ERROR: WIFI INITIALIZE FAIL.\r\n");
+        printf("ERROR: WiFi initialize fail.\r\n");
         return status;
     }
+
+    // Initialize TLS
+    nx_secure_tls_initialize();
 
     // Create DNS.
     status = dns_create();
@@ -207,12 +212,9 @@ int stm32_network_init(void)
     {
         nx_ip_delete(&ip_0);
         nx_packet_pool_delete(&main_pool);
-        printf("ERROR: DNS CREATE FAIL.\r\n");
+        printf("ERROR: DNS create fail.\r\n");
         return status;
     }
-
-    // Initialize TLS
-    nx_secure_tls_initialize();
 
     return NX_SUCCESS;
 }
@@ -224,7 +226,7 @@ unsigned long sntp_get_time(void)
     ULONG tx_time_delta = (tx_time_get() - tx_last_ticks) / TX_TIMER_TICKS_PER_SECOND;
 
     // Add this to the last sync time to get the current time
-    ULONG current_time = 1590969600 + tx_time_delta;
+    ULONG current_time = 1609459200 + tx_time_delta;
 
     return current_time;
 }
