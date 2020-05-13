2263cea1c6304aa29afa7d23ef41dcd5d731ee7e
Ryan Winter <ryanwinter@outlook.com>
2020-05-13 00:04:07 -0700

    updated rebuild.bat for Microchip to use build dir instead of _build for project consistency

--- Microchip/ATSAME54-XPRO/tools/flash.bat
+++ Microchip/ATSAME54-XPRO/tools/flash.bat
@@ -1,1 +1,1 @@
-c:\work\AzureIoT\OpenOCD\bin\openocd -f c:\work\azureiot\openocd\share\openocd\scripts\board\microchip_same54_xplained_pro.cfg -c "program _build/app/atsame54_azure_iot.bin verify reset exit"
+c:\work\AzureIoT\OpenOCD\bin\openocd -f c:\work\azureiot\openocd\share\openocd\scripts\board\microchip_same54_xplained_pro.cfg -c "program build/app/atsame54_azure_iot.bin verify reset exit"
--- Microchip/ATSAME54-XPRO/tools/rebuild.bat
+++ Microchip/ATSAME54-XPRO/tools/rebuild.bat
@@ -3,9 +3,9 @@
 setlocal
 cd /d %~dp0\..
 
-IF EXIST build (rd /S /Q _build)
+IF EXIST build (rd /S /Q build)
 
-cmake -B_build -GNinja -DCMAKE_TOOLCHAIN_FILE="../../cmake/arm-gcc-cortex-m4.cmake" -DCPM_SOURCE_CACHE="%USERPROFILE%/.cpm"
-cmake --build _build
+cmake -Bbuild -GNinja -DCMAKE_TOOLCHAIN_FILE="../../cmake/arm-gcc-cortex-m4.cmake" -DCPM_SOURCE_CACHE="%USERPROFILE%/.cpm"
+cmake --build build
 
 IF %0 == "%~0" pause
