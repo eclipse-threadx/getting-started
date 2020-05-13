79b6137a94b38d12d4fc791910ceef6bd898bb83
Mariana Mihova <marine@microsoft.com>
2020-05-13 14:19:51 -0700

    Update path in flash.bat

--- Microchip/ATSAME54-XPRO/tools/flash.bat
+++ Microchip/ATSAME54-XPRO/tools/flash.bat
@@ -1,1 +1,1 @@
-atprogram.exe --tool edbg --interface SWD --device ATSAME54P20A program --chiperase --file C:\Work\Git\GSGGit\getting-started\Microchip\ATSAME54-XPRO\build\app\atsame54_azure_iot.bin --verify
+atprogram.exe --tool edbg --interface SWD --device ATSAME54P20A program --chiperase --file C:\Work\Git\getting-started\Microchip\ATSAME54-XPRO\build\app\atsame54_azure_iot.bin --verify
