b004f5f37983013cfc4410df856078fd4dd33d66
Mariana Mihova <marine@microsoft.com>
2020-05-14 13:49:22 -0700

    Move __SENSOR_BME280__ to azure_config.h

--- Microchip/ATSAME54-XPRO/app/azure_config.h
+++ Microchip/ATSAME54-XPRO/app/azure_config.h
@@ -6,4 +6,10 @@
 #define IOT_DEVICE_ID       ""
 #define IOT_PRIMARY_KEY     ""
 
+// If the BME280 sensor is available
+// uncomment the lines below to get real temperature readings
+//#ifndef __SENSOR_BME280__
+//#define __SENSOR_BME280__
+//#endif
+
 #endif // _AZURE_CONFIG_H
