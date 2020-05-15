168256c78e29ebcba44c7433cea59f948d687b14
Ryan Winter <ryanwinter@outlook.com>
2020-05-14 22:09:12 -0700

    roll out settings

--- Microchip/ATSAME54-XPRO/app/azure_config.h
+++ Microchip/ATSAME54-XPRO/app/azure_config.h
@@ -2,8 +2,8 @@
 #define _AZURE_CONFIG_H
 
 /*************** Azure IoT Hub Connection Configuration ******************/
-#define IOT_HUB_HOSTNAME    "azurertos-iot.azure-devices.net"
-#define IOT_DEVICE_ID       "SAME54"
-#define IOT_PRIMARY_KEY     "QhTpPmqJzkKjyDYTanabbnEl4UYwyP0CCqdu8JcUaow="
+#define IOT_HUB_HOSTNAME    ""
+#define IOT_DEVICE_ID       ""
+#define IOT_PRIMARY_KEY     ""
 
 #endif // _AZURE_CONFIG_H
