7522f041631d4b3214485b2fa376cccfc3ba6015
Ryan Winter <ryanwinter@outlook.com>
2020-05-21 15:48:03 -0700

    actions

--- STMicroelectronics/B-L475E-IOT01A/app/azure_config.h
+++ STMicroelectronics/B-L475E-IOT01A/app/azure_config.h
@@ -15,15 +15,15 @@ typedef enum
 // ----------------------------------------------------------------------------
 // WiFi connection information
 // ----------------------------------------------------------------------------
-#define WIFI_SSID           ""
-#define WIFI_PASSWORD       ""
+#define WIFI_SSID           "ferret"
+#define WIFI_PASSWORD       "i am a furry animal"
 #define WIFI_MODE           WPA2_PSK_AES
 
 // ----------------------------------------------------------------------------
 // Azure IoT Device information
 // ----------------------------------------------------------------------------
-#define IOT_HUB_HOSTNAME    ""
-#define IOT_DEVICE_ID       ""
-#define IOT_PRIMARY_KEY     ""
+#define IOT_HUB_HOSTNAME    "ryantest2.azure-devices.net"
+#define IOT_DEVICE_ID       "stm"
+#define IOT_PRIMARY_KEY     "eNVewYP9j627+BPsoqxh9NAH+0zyePBXRW84TS10YpE="
 
 #endif // _AZURE_CONFIG_H
