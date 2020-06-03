aed59d367bd927b59a48267351ee23bef21c14e6
Ryan Winter <ryanwinter@outlook.com>
2020-06-02 20:44:20 -0700

    Provide warning if STM wifi modules is running outdated firmware

--- common/newlib_nano.c
+++ common/newlib_nano.c
@@ -24,7 +24,8 @@ caddr_t _sbrk(int incr)
 	static unsigned char *heap = NULL;
 	unsigned char *       prev_heap;
 
-	if (heap == NULL) {
+	if (heap == NULL) 
+    {
 		heap = (unsigned char *)&_end;
 	}
 	prev_heap = heap;
