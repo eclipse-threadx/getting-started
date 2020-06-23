e516b8459791440c28f73a5b22661bff789406ce
Bhupendra Naphade <bhnaphad@microsoft.com>
2020-06-23 17:30:44 +0200

    cleared credentials

--- MXChip/AZ3166/app/azure_config.h
+++ MXChip/AZ3166/app/azure_config.h
@@ -9,9 +9,9 @@
 // ----------------------------------------------------------------------------
 // WiFi connection information
 // ----------------------------------------------------------------------------
-#define WIFI_SSID           "FRITZ!Box 6490 Cable"
+#define WIFI_SSID           ""
 
-#define WIFI_PASSWORD       "42732488687857862742"
+#define WIFI_PASSWORD       ""
 
 /* WIFI Security, the security types are defined in wwwd_constants.h.  */
 #define WIFI_SECURITY       WICED_SECURITY_WPA2_MIXED_PSK
@@ -21,8 +21,8 @@
 // ----------------------------------------------------------------------------
 // Azure IoT Device information
 // ----------------------------------------------------------------------------
-#define IOT_HUB_HOSTNAME    "azurertos-test.azure-devices.net"
-#define IOT_DEVICE_ID       "bhnaphad-test"
-#define IOT_PRIMARY_KEY     "gbH78eY+2ovz9BeGGt308bpJ0WS7+jG3HGJR6NMNjPU="
+#define IOT_HUB_HOSTNAME    ""
+#define IOT_DEVICE_ID       ""
+#define IOT_PRIMARY_KEY     ""
 
 #endif // _AZURE_CONFIG_H
