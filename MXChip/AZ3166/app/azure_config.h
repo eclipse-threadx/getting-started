bf521873e1dadae02eec7c71c4cc79214cd2ed58
Liya Du <liydu@users.noreply.github.com>
2020-07-03 05:21:19 +0800

    Enlarge RX packet count (#91)

--- MXChip/AZ3166/app/azure_config.h
+++ MXChip/AZ3166/app/azure_config.h
@@ -9,8 +9,8 @@
 // ----------------------------------------------------------------------------
 // WiFi connection information
 // ----------------------------------------------------------------------------
-#define WIFI_SSID           "iot_test"
-#define WIFI_PASSWORD       "iot_rulez"
+#define WIFI_SSID           ""
+#define WIFI_PASSWORD       ""
 
 // WIFI Security, the security types are defined in wwd_constants.h
 #define WIFI_SECURITY       WICED_SECURITY_WPA2_MIXED_PSK
@@ -20,8 +20,8 @@
 // ----------------------------------------------------------------------------
 // Azure IoT Device information
 // ----------------------------------------------------------------------------
-#define IOT_HUB_HOSTNAME    "ryangsg.azure-devices.net"
-#define IOT_DEVICE_ID       "mxchip"
-#define IOT_PRIMARY_KEY     "6v4WRLRV0od3oJo+So+PCP54TxoS12ZGiM/myyRm6PY="
+#define IOT_HUB_HOSTNAME    ""
+#define IOT_DEVICE_ID       ""
+#define IOT_PRIMARY_KEY     ""
 
 #endif // _AZURE_CONFIG_H
