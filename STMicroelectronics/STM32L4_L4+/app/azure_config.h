56aadb663d662283142174f334c5d090408b6f3f
Ryan Winter <ryanwinter@outlook.com>
2020-07-07 14:23:51 -0700

    Add STM azure_config.h

---
+++ STMicroelectronics/STM32L4_L4+/app/azure_config.h
@@ -0,0 +1,36 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+   
+#ifndef _AZURE_CONFIG_H
+#define _AZURE_CONFIG_H
+
+typedef enum
+{
+    None          = 0, 
+    WEP           = 1,
+    WPA_PSK_TKIP  = 2,
+    WPA2_PSK_AES  = 3
+} WiFi_Mode;
+
+// ----------------------------------------------------------------------------
+// WiFi connection information
+// ----------------------------------------------------------------------------
+#define WIFI_SSID           ""
+#define WIFI_PASSWORD       ""
+#define WIFI_MODE           WPA2_PSK_AES
+
+// ----------------------------------------------------------------------------
+// Azure IoT Hub Connection Transport
+// Define this to use the nx client, otherwise MQTT
+// ----------------------------------------------------------------------------
+//#define USE_NX_CLIENT_PREVIEW
+
+// ----------------------------------------------------------------------------
+// Azure IoT Device information
+// ----------------------------------------------------------------------------
+#define IOT_HUB_HOSTNAME    ""
+#define IOT_DEVICE_ID       ""
+#define IOT_PRIMARY_KEY     ""
+
+#endif // _AZURE_CONFIG_H
+
