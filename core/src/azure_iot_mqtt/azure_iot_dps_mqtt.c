f2afd142430b249c63a149c7d2f167b4ea3d85de
Ryan Winter <ryanwinter@outlook.com>
2020-09-17 00:05:33 -0700

    always send model id for DPS

--- core/src/azure_iot_mqtt/azure_iot_dps_mqtt.c
+++ core/src/azure_iot_mqtt/azure_iot_dps_mqtt.c
@@ -307,21 +307,11 @@ UINT azure_iot_dps_register(AZURE_IOT_MQTT* azure_iot_mqtt, UINT wait)
     }
 
     // Create the payload
-    if (azure_iot_mqtt->mqtt_model_id == 0)
-    {
-        snprintf(mqtt_publish_payload,
-            sizeof(mqtt_publish_payload),
-            "{\"registrationId\":\"%s\"}",
-            azure_iot_mqtt->mqtt_dps_registration_id);
-    }
-    else
-    {
-        snprintf(mqtt_publish_payload,
-            sizeof(mqtt_publish_payload),
-            "{\"registrationId\":\"%s\",\"payload\":{\"modelId\":\"%s\"}}",
-            azure_iot_mqtt->mqtt_dps_registration_id,
-            azure_iot_mqtt->mqtt_model_id);
-    }
+    snprintf(mqtt_publish_payload,
+        sizeof(mqtt_publish_payload),
+        "{\"registrationId\":\"%s\",\"payload\":{\"modelId\":\"%s\"}}",
+        azure_iot_mqtt->mqtt_dps_registration_id,
+        azure_iot_mqtt->mqtt_model_id);
 
     // Register the device
     status = mqtt_publish(azure_iot_mqtt, DPS_REGISTER_TOPIC, mqtt_publish_payload);
