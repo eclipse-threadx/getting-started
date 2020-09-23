9ea57317e0396314242c015e17757474a1990c6d
Ryan Winter <ryanwinter@outlook.com>
2020-09-22 17:10:11 -0700

    revert changes to mirror sdk defaults

--- NXP/MIMXRT1060-EVK/app/board_init.c
+++ NXP/MIMXRT1060-EVK/app/board_init.c
@@ -1,17 +1,16 @@
 /* Copyright (c) Microsoft Corporation.
    Licensed under the MIT License. */
-   
+
 #include "board_init.h"
 
 #include "board.h"
-#include "pin_mux.h"
 #include "peripherals.h"
+#include "pin_mux.h"
 
 void board_init()
 {
-	// init board hardware
+    // init board hardware
     BOARD_InitBootPins();
-    BOARD_InitENET();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
 
