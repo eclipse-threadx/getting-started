cc7449e41ee194a1808168ed83ffccaeb880eeb8
Ryan Winter <ryanwinter@outlook.com>
2020-09-23 12:13:10 -0700

    Move ninja into the repo until we resolve installation security prompts.

--- cmake/arm-gcc-cortex-m7.cmake
+++ cmake/arm-gcc-cortex-m7.cmake
@@ -6,4 +6,4 @@ set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv5-sp-d16")
 set(LD_FLAGS "")
 set(SPECS_FLAGS "--specs=nano.specs -u _printf_float")
 
-include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-toolchain.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-cortex-toolchain.cmake)
