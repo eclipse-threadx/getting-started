64342016b0748b4df9b0d586c7e34ccfe9d8888e
Ryan Winter <rywinter@microsoft.com>
2020-10-21 12:40:40 -0700

    toolchain optimisations, mxchip

--- cmake/arm-gcc-cortex-m4.cmake
+++ cmake/arm-gcc-cortex-m4.cmake
@@ -3,7 +3,5 @@
 
 set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")
 set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
-set(LD_FLAGS "")
-set(SPECS_FLAGS "--specs=nano.specs -u _printf_float")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-cortex-toolchain.cmake)
--- core/src/azure_iot_mqtt/azure_iot_mqtt.c
+++ core/src/azure_iot_mqtt/azure_iot_mqtt.c
@@ -191,7 +191,10 @@ static UINT mqtt_publish_float(AZURE_IOT_MQTT* azure_iot_mqtt, CHAR* topic, CHAR
 {
     CHAR mqtt_message[100];
 
-    snprintf(mqtt_message, sizeof(mqtt_message), "{\"%s\":%3.2f}", label, value);
+    int decvalue = value;
+    int fracvalue = abs(100 * (value - (long)value));
+    
+    snprintf(mqtt_message, sizeof(mqtt_message), "{\"%s\":%d.%2d}", label, decvalue, fracvalue);
     printf("Sending message %s\r\n", mqtt_message);
 
     return mqtt_publish(azure_iot_mqtt, topic, mqtt_message);
