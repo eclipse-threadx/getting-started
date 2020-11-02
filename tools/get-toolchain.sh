1e38e98fb3aa11c33a6f3443e9f359c32b4dccdd
Mete Balci <metebalci@gmail.com>
2020-11-02 20:43:04 +0100

    fix to support different ubuntu release codenames (#157)

--- tools/get-toolchain.sh
+++ tools/get-toolchain.sh
@@ -8,7 +8,8 @@ sudo apt-get install apt-transport-https ca-certificates gnupg software-properti
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
 
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+CODENAME=$(lsb_release -c | cut -f2 -d':' | sed 's/\t//')
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $CODENAME main"
 
 sudo apt-get update
 sudo apt-get install  gcc-arm-none-eabi ninja-build cmake 
