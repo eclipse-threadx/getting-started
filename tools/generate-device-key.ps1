6f0ac0de73f59ac376564e1f34f43d821bc60224
Ryan Winter <ryanwinter@outlook.com>
2020-11-11 14:09:45 -0800

    Add device key generator

---
+++ tools/generate-device-key.ps1
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+param(
+    [Parameter(Mandatory=$true)][string]$key, 
+    [Parameter(Mandatory=$true)][string]$id
+)
+
+$hmacsha = New-Object System.Security.Cryptography.HMACSHA256
+$hmacsha.key = [Convert]::FromBase64String($key)
+
+$deviceKey = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($id))
+$deviceKey = [Convert]::ToBase64String($deviceKey)
+
+Write-Output $deviceKey
