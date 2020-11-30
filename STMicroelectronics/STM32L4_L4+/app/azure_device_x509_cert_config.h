cac5e9d3d8f56296e80c5cdacb23fd1eec59ac90
Ryan Winter <ryanwinter@outlook.com>
2020-11-30 13:38:46 -0800

    add license header to cert config file

--- STMicroelectronics/STM32L4_L4+/app/azure_device_x509_cert_config.h
+++ STMicroelectronics/STM32L4_L4+/app/azure_device_x509_cert_config.h
@@ -1,3 +1,6 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
 #ifndef _AZURE_DEVICE_X509_CERT_CONFIG_H
 #define _AZURE_DEVICE_X509_CERT_CONFIG_H
 
@@ -6,13 +9,13 @@
 // Replace {0x00} with your formatted output from OpenSSL and xxd here
 // ----------------------------------------------------------------------------
 const unsigned char iot_x509_device_cert[] = {0x00};
-unsigned int iot_x509_device_cert_len = sizeof(iot_x509_device_cert);
+unsigned int iot_x509_device_cert_len      = sizeof(iot_x509_device_cert);
 
 // ----------------------------------------------------------------------------
 // Azure IoT X509 Device Private Key
 // Replace {0x00} with your formatted output from OpenSSL and xxd here
 // ----------------------------------------------------------------------------
-unsigned char iot_x509_private_key[] = {0x00};
+unsigned char iot_x509_private_key[]        = {0x00};
 const unsigned int iot_x509_private_key_len = sizeof(iot_x509_private_key);
 
 #endif
