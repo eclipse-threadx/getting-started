885c4e2296fd0bda19bdb82db9fed74059d3f5ab
Ryan Winter <ryanwinter@outlook.com>
2020-12-08 22:24:28 -0800

    Add RX65N app files

--- NXP/MIMXRT1060-EVK/app/board_init.h
+++ NXP/MIMXRT1060-EVK/app/board_init.h
@@ -4,8 +4,6 @@
 #ifndef _BOARD_INIT_H
 #define _BOARD_INIT_H
 
-#include "nx_api.h"
-
 void board_init();
 
 #endif // _BOARD_INIT_H
---
+++ Renesas/RX65N/app/board_init.c
@@ -0,0 +1,8 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
+#include "board_init.h"
+
+void board_init()
+{
+}
