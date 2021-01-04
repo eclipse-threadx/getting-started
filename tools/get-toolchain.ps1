da6bf1d0b0bca79ad291f7eae712bb04f815e2d2
Ryan Winter <ryanwinter@outlook.com>
2021-01-04 14:46:43 -0800

    update toolchain installs

--- tools/get-toolchain.ps1
+++ tools/get-toolchain.ps1
@@ -3,15 +3,15 @@
 
 echo "`nInstalling prerequisites. Please leave the window open until the installation completes."
 
-$cmake_path = 'https://github.com/Kitware/CMake/releases/download/v3.19.0'
-$cmake_file = 'cmake-3.19.0-win32-x86.msi'
-$cmake_name = 'CMake v3.19.0'
-$cmake_hash = 'D2F2163AC6409DC05A46A483264A04CF0569457C765B8DD01C6EFA3642498975'
+$cmake_path = 'https://github.com/Kitware/CMake/releases/download/v3.19.2'
+$cmake_file = 'cmake-3.19.2-win32-x86.msi'
+$cmake_name = 'CMake v3.19.2'
+$cmake_hash = '7ABFEA6278B7BF3F6EB1D3B13F9DF79FD675D274AC88A680831B81DF26C92611'
 
-$gccarm_path = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2'
-$gccarm_file = 'gcc-arm-none-eabi-9-2020-q2-update-win32.exe'
-$gccarm_name = 'GCC-ARM 9-2020-q2-update'
-$gccarm_hash = '2BFF61698B1EBDE0B191D169B16934B7BB6AE624FC493FFDE193E35019726940'
+$gccarm_path = 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/'
+$gccarm_file = 'gcc-arm-none-eabi-10-2020-q4-major-win32.exe'
+$gccarm_name = 'GCC-ARM 10-2020-q4-major'
+$gccarm_hash = '9754D95905288AFC787837C3B211A3E502A9EDE8A5F544CFD369C30FEA509CA5'
 
 $termite_path = 'https://www.compuphase.com/software'
 $termite_file = 'termite-3.4.exe'
@@ -19,9 +19,9 @@ $termite_name = 'Termite v3.4'
 $termite_hash = 'CA440B6C7F6EAA812BA5F8BF42AED86E02022CA50A1C72585168C9B671D0FE19'
 
 $azure_cli_path = 'https://azurecliprod.blob.core.windows.net/msi'
-$azure_cli_file = 'azure-cli-2.15.0.msi'
-$azure_cli_name = 'Azure CLI v2.15.0'
-$azure_cli_hash = 'F3ACD85F36560D3F48BA7CCE0BA3194F74B7195EBA3117F960CE094128812A20'
+$azure_cli_file = 'azure-cli-2.17.1.msi'
+$azure_cli_name = 'Azure CLI v2.17.1'
+$azure_cli_hash = '414B5DC8030A6E1755650CA4966417735E42D1FCC89FE9D2E41C5720BC88AEA3'
 
 $iot_explorer_path = 'https://github.com/Azure/azure-iot-explorer/releases/download/v0.13.0'
 $iot_explorer_file = 'Azure.IoT.Explorer.preview.0.13.0.msi'
