49f106424b104a0bcdc6fb43f9fa1bb10d4aeb6f
R. de Veen <rdeveen@users.noreply.github.com>
2021-02-10 20:25:33 +0100

    Update ssd1306.h
    Fix small typo.

--- MXChip/AZ3166/lib/mxchip_bsp/ssd1306/Inc/ssd1306.h
+++ MXChip/AZ3166/lib/mxchip_bsp/ssd1306/Inc/ssd1306.h
@@ -95,7 +95,7 @@ extern SPI_HandleTypeDef SSD1306_SPI_PORT;
 #define SSD1306_WIDTH           128
 #endif
 
-// some LEDs don't display anything in first two columns
+// some OLEDs don't display anything in first two columns
 // #define SSD1306_WIDTH           130
 
 // Enumeration for screen colors
