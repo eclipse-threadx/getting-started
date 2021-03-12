b02af1962415d6a1fa552bfa0b8e9dcfda33db42
Jimmy Brisson <jimmy.brisson@linaro.org>
2021-03-12 14:42:55 -0600

    Build ThreadX into the MPS3-AN524 example
    This is a rollup of the following changes:
     * Use ThreadX and threads in the example.
     * Build ThreadX.
     * Use the threadx cortex_m33.cmake build file instead of the one in
     this repo to avoid falling out of sync with upstream.
     * Update the tfm submodule reference to point to linaro/lite-azure-threadx.
    
    Signed-off-by: Jimmy Brisson <jimmy.brisson@linaro.org>
    Signed-off-by: Kevin Townsend <kevin.townsend@linaro.org>

--- Arm/MPS3_AN524/app/main.c
+++ Arm/MPS3_AN524/app/main.c
@@ -2,7 +2,61 @@
 
 #include <stdio.h>
 
+#include "tx_api.h"
+
 #include "board_init.h"
+#include "cmsis.h"
+
+#define AZURE_THREAD_STACK_SIZE 4096
+#define AZURE_THREAD_PRIORITY   4
+
+static __inline void systick_interval_set(uint32_t ticks)
+{
+    // 1. Disable the counter
+    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+
+    // 2. Update the Systick timer period
+    SysTick->LOAD = SystemCoreClock / ticks - 1;
+
+    // 3. Clear the current value
+    SysTick->VAL  = 0;
+
+    // 4. Enable the counter
+    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+}
+
+TX_THREAD azure_thread;
+ULONG azure_thread_stack[AZURE_THREAD_STACK_SIZE / sizeof(ULONG)];
+
+void azure_thread_entry(ULONG parameter);
+void tx_application_define(void* first_unused_memory);
+
+void azure_thread_entry(ULONG parameter)
+{
+    printf("\r\nStarting Azure thread\r\n\r\n");
+}
+
+void tx_application_define(void* first_unused_memory)
+{
+    systick_interval_set(TX_TIMER_TICKS_PER_SECOND);
+
+    // Create Azure thread
+    UINT status = tx_thread_create(&azure_thread,
+        "Azure Thread",
+        azure_thread_entry,
+        0,
+        azure_thread_stack,
+        AZURE_THREAD_STACK_SIZE,
+        AZURE_THREAD_PRIORITY,
+        AZURE_THREAD_PRIORITY,
+        TX_NO_TIME_SLICE,
+        TX_AUTO_START);
+
+    if (status != TX_SUCCESS)
+    {
+        printf("Azure IoT thread creation failed\r\n");
+    }
+}
 
 int main(void)
 {
@@ -12,7 +66,7 @@ int main(void)
     printf("Azure RTOS running on MPS3 board\n");
 
     
-    /* tx_kernel_enter(); */
+    tx_kernel_enter();
 
     return 0;
 }
