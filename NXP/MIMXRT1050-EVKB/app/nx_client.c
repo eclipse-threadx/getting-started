3b3a387e7506a108290360c37df09425c3052a95
Ryan Winter <ryanwinter@outlook.com>
2021-03-29 14:03:37 -0700

    add temperature monitoring for the MCU (#223)
    * add temperature monitoring for the MCU
    
    * update readme with temperature definition

--- NXP/MIMXRT1050-EVKB/app/nx_client.c
+++ NXP/MIMXRT1050-EVKB/app/nx_client.c
@@ -17,6 +17,8 @@
 #include "azure_device_x509_cert_config.h"
 #include "azure_pnp_info.h"
 
+#include "fsl_tempmon.h"
+
 #define IOT_MODEL_ID "dtmi:azurertos:devkit:gsg;1"
 
 #define TELEMETRY_TEMPERATURE       "temperature"
@@ -82,7 +84,9 @@ static UINT append_device_info_properties(NX_AZURE_IOT_JSON_WRITER* json_writer,
 
 static UINT append_device_telemetry(NX_AZURE_IOT_JSON_WRITER* json_writer, VOID* context)
 {
-    const float temperature = 28.5;
+    TEMPMON_StartMeasure(TEMPMON);
+    float temperature = TEMPMON_GetCurrentTemperature(TEMPMON);
+    TEMPMON_StopMeasure(TEMPMON);
 
     if (nx_azure_iot_json_writer_append_property_with_double_value(
             json_writer, (UCHAR*)TELEMETRY_TEMPERATURE, sizeof(TELEMETRY_TEMPERATURE) - 1, temperature, 2))
