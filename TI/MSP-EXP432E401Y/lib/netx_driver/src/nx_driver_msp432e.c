587213edaa2ab501fb41b30740fb030795bfb082
David Vescovi <dvescovi@tampabay.rr.com>
2021-04-05 09:08:19 -0400

    driver tweak

--- TI/MSP-EXP432E401Y/lib/netx_driver/src/nx_driver_msp432e.c
+++ TI/MSP-EXP432E401Y/lib/netx_driver/src/nx_driver_msp432e.c
@@ -1393,9 +1393,7 @@ uint16_t ui16Val;
             nx_driver_information.nx_driver_information_dma_rx_descriptors[i].ui32Count = DES1_RX_CTRL_CHAINED;
             nx_driver_information.nx_driver_information_dma_rx_descriptors[i].ui32Count |= ((packet_ptr -> nx_packet_data_end - packet_ptr -> nx_packet_data_start) <<
                                                                                             DES1_RX_CTRL_BUFF1_SIZE_S); 
-//            nx_driver_information.nx_driver_information_dma_rx_descriptors[i].ui32Count |= (DSBUF_SIZE << DES1_RX_CTRL_BUFF1_SIZE_S); 
             nx_driver_information.nx_driver_information_dma_rx_descriptors[i].pvBuffer1 = packet_ptr->nx_packet_prepend_ptr;
-//            nx_driver_information.nx_driver_information_dma_rx_descriptors[i].ui32CtrlStatus = 0;
             nx_driver_information.nx_driver_information_dma_rx_descriptors[i].ui32CtrlStatus = DES0_RX_CTRL_OWN;
             nx_driver_information.nx_driver_information_dma_rx_descriptors[i].DES3.pLink =
                 ((i == (NX_DRIVER_RX_DESCRIPTORS - 1)) ?
@@ -2011,7 +2009,6 @@ NX_PACKET     *received_packet_ptr = nx_driver_information.nx_driver_information
                 
                 /* Store the length of the packet in the first NX_PACKET.  */
                 nx_driver_information.nx_driver_information_receive_packets[first_idx] -> nx_packet_length = ((nx_driver_information.nx_driver_information_dma_rx_descriptors[idx].ui32CtrlStatus & DES0_RX_STAT_FRAME_LENGTH_M) >> DES0_RX_STAT_FRAME_LENGTH_S) - 4;
-//                nx_driver_information.nx_driver_information_receive_packets[first_idx] -> nx_packet_length = (nx_driver_information.nx_driver_information_dma_rx_descriptors[idx].ui32CtrlStatus & DES0_RX_STAT_FRAME_LENGTH_M) >> DES0_RX_STAT_FRAME_LENGTH_S;
 
                 /* Adjust nx_packet_append_ptr with the size of the data in this buffer.  */
                 nx_driver_information.nx_driver_information_receive_packets[idx] -> nx_packet_append_ptr = nx_driver_information.nx_driver_information_receive_packets[idx] -> nx_packet_prepend_ptr
