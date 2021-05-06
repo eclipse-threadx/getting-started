6cd95f11c0894a1e6643a486e9dc9a8879527a18
Ryan Winter <ryanwinter@outlook.com>
2021-05-06 14:16:16 -0700

    collapse elf extension into target linker function

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -23,6 +23,8 @@ function(set_target_linker TARGET LINKER_SCRIPT)
     elseif(CMAKE_C_COMPILER_ID STREQUAL "GNU")
         target_link_options(${TARGET} PRIVATE -T${LINKER_SCRIPT})
         target_link_options(${TARGET} PRIVATE -Wl,-Map=${TARGET}.map)
+        set_target_properties(${TARGET} PROPERTIES SUFFIX ".elf") 
+
     else()
         message(FATAL_ERROR "Unknown CMAKE_C_COMPILER_ID ${CMAKE_C_COMPILER_ID}")
     endif()
