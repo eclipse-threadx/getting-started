88cf997feb16cd3cb1f6def39f63b3069bfa59c3
Ryan Winter <ryanwinter@outlook.com>
2021-12-01 14:33:11 -0800

    remove old STM guides and replaced with link to new ones

--- STMicroelectronics/STM32L4_L4+/tools/flash.bat
+++
@@ -1,7 +0,0 @@
-:: Copyright (c) Microsoft Corporation.
-:: Licensed under the MIT License.
-
-setlocal
-cd /d %~dp0\..
-
-openocd -f board/stm32l4discovery.cfg -c "program build/app/stm32l475_azure_iot.elf verify reset exit"
