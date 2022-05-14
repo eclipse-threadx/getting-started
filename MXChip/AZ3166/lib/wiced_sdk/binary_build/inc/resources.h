f56af271f9e08f0d57a89c574d80e3becbd217ee
TiejunZhou <50469179+TiejunMS@users.noreply.github.com>
2022-05-14 14:54:27 +0800

    Update Azure RTOS to 6.1.11 (#332)
    * Update Azure RTOS to 6.1.11
    
    * Update libwiced_sdk_bin.a to switch to debug and add strip stage.
    
    Co-authored-by: Ryan Winter <ryanwinter@outlook.com>

---
+++ MXChip/AZ3166/lib/wiced_sdk/binary_build/inc/resources.h
@@ -0,0 +1,10 @@
+/* Automatically generated file - this comment ensures resources.h file creation */ 
+/* Auto-generated header file. Do not edit */
+#ifndef INCLUDED_RESOURCES_H_
+#define INCLUDED_RESOURCES_H_
+#include "wiced_resource.h"
+
+extern const resource_hnd_t resources_firmware_DIR_43362_DIR_43362A2_bin;
+extern const char resources_firmware_DIR_43362_DIR_43362A2_bin_data[213732];
+
+#endif /* ifndef INCLUDED_RESOURCES_H_ */
